aa539aedb1ca45481ff7e55d98d4804c89d27ff0
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2026-05-14 17:36:25 +0300

    Initial commit of CodeIgniter project

---
+++ application/views/templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://bootswatch.com/5/journal/bootstrap.min.css">
+  <title>ciblog</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <span class="navbar-header">
+        <a href="" class="navbar-brand">ciblog</a>
+      </span>
+      <div id="navbar">
+        <ul class="nav d-flex flex-row gap-3 navbar-nav">
+          <li><a href="<?php echo base_url(); ?>home">Home</a></li>
+          <li><a href="<?php echo base_url(); ?>about">About</a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
