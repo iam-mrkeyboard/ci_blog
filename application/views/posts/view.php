b3e2b3d05065efcce528a5195cee06e01c5904b6
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2026-05-15 13:27:05 +0300

    Fix typo and design posts/view page

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,1 +1,12 @@
-<h1><?= $title ?></h1>
+<div class="row">
+  <div class="col-md-8 offset-md-2">
+    <h2><?= $title ?></h2>
+    <p class="text-muted">Posted on <?= date('F j, Y', strtotime($post['created_at'])) ?></p>
+    <hr>
+    <div class="post-body">
+      <?= $post['body'] ?>
+    </div>
+    <hr>
+    <a href="<?= site_url('posts') ?>" class="btn btn-secondary">Back to Posts</a>
+  </div>
+</div>
